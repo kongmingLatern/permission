168e4690d05a703ee4ba7c5e27ccb5fa6d94763d
tangshixian <fuxia@sct1039.com>
2024-03-04 17:12:57 +0800

    feat: add icons and finish basic Admin layout

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import 'uno.css'
+import 'vfonts/FiraCode.css'
 
 import App from './App.vue'
 import { createApp } from 'vue'
--- src/modules/Admin/pages/index.vue
+++ src/modules/Admin/pages/index.vue
@@ -1,5 +1,20 @@
-<template>Admin</template>
+<template>
+	<n-layout has-sider>
+		<n-layout-sider h-screen>
+			<AdminMenu />
+		</n-layout-sider>
+		<n-layout>
+			<n-layout-header>颐和园路</n-layout-header>
+			<n-layout-content content-style="padding: 24px;">
+				平山道
+			</n-layout-content>
+			<n-layout-footer>成府路</n-layout-footer>
+		</n-layout>
+	</n-layout>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AdminMenu from '@/modules/Admin/components/AdminMenu.vue'
+</script>
 
 <style scoped></style>
